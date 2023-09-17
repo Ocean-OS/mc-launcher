52411fc971322de5493f8a759df2e5fff4a7b5c2
ComputerGuy <63362464+Ocean-OS@users.noreply.github.com>
2023-09-17 14:50:15 -0700

    Upload files for 0.2.5 Alpha

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Tray, contextBridge, ipcRenderer} = require('electron');
+const { app, BrowserWindow, Tray, contextBridge, ipcMain} = require('electron');
 const nativeImage = require('electron').nativeImage;
 process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = 'true';
 const path = require('path');
@@ -17,18 +17,34 @@ function createWindow () {
       contextIsolation: false,
     }
   })
+  // and load the index.html of the app.
   mainWindow.maximize();
   mainWindow.setTitle("Loading...");
   mainWindow.loadFile('index.html');
+  ipcMain.on('loadStatus', (event, args) => {
+    mainWindow.setProgressBar(args);
+  });
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
 }
-
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow()
-
   app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const {ipcRenderer} = require('electron');
 function check(){
     if(window.filesDownloaded == window.versionFiles[window.versionNames.indexOf(window.selection.name)].length){
         try{
@@ -14,59 +15,74 @@ async function getFile(url, filename) {
   const data = await response.text();
   fs.writeFileSync(filename, data);
 }
+async function checkUpdates(){
+    var response = await fetch('https://ocean-os.github.io/mc-launcher/version.txt');
+    var data = await response.text();
+    if(!fs.existsSync(path.join(__dirname, "version.txt")) || fs.readFileSync(path.join(__dirname, "version.txt"), "utf8") !== data){
+        if(confirm("There is an update available: v" + data + "\n Would you like to install it?")){
+        fs.writeFileSync(path.join(__dirname, "version.txt"), data);
+        var res = await fetch("https://ocean-os.github.io/mc-launcher/dir.js");
+        var updateFiles = await res.text();
+        updateFiles = eval(updateFiles);
+        var updateDownload = 0;
+        while(updateDownload < updateFiles.length){
+            var res = await fetch("https://ocean-os.github.io/mc-launcher/" + updateFiles[updateDownload].replace("./", ""));
+            var fileSave = await res.text();
+            fs.writeFileSync(path.join(__dirname, updateFiles[updateDownload].replace("./", "")), fileSave);
+            ipcRenderer.send('loadStatus', updateDownload/updateFiles.length);
+            updateDownload++;
+            if(updateDownload == updateFiles.length){
+                ipcRenderer.send("loadStatus", 0-1);
+                alert("To run the updated version, the launcher will refresh. ");
+                window.location.reload();
+            }
+        }
+        
+        }
+    }
+}
+function loadVersions(){
+    var versionNames = [];
+    var versionUrls = [];
+    var versionFolders = [];
+    var versionFiles = [];
+    var versionChangelogs = [];
+    for(var versionAdd = window.versionList.length-1; versionAdd > -1; versionAdd--){
+        versionChangelogs.push(window.versionList[versionAdd].changelog);
+        versionNames.push(window.versionList[versionAdd].version);
+        versionFolders.push(window.versionList[versionAdd].folder);
+        versionFiles.push(window.versionList[versionAdd].files);
+        versionUrls.push(window.versionList[versionAdd].url);
+        var versionAddElement = document.createElement("option");
+        versionAddElement.name = window.versionList[versionAdd].version; 
+        versionAddElement.value = window.versionList[versionAdd].version;
+        versionAddElement.innerText = window.versionList[versionAdd].version;
+        versionAddElement.id = window.versionList[versionAdd].url;
+        document.getElementById("versionSelect").appendChild(versionAddElement);
+    }
+    window.versionUrls = versionUrls;
+    window.versionFiles = versionFiles;
+    window.versionFolders = versionFolders;
+    window.versionNames = versionNames;
+    window.versionChangelogs = versionChangelogs;
+    document.getElementById("changelog").innerHTML = "<h3>Changelog for Mine.craft " +      window.versionNames[0] + "</h3><p>" + window.versionList[window.versionList.length-1].changelog + "</p>";
+}
 async function getVersions() {
     var response = await fetch('https://ocean-os.github.io/mc-assets/versions.js');
     var data = await response.text();
-    fs.writeFileSync(__dirname + "/versions.js", data);
-    window.versionList = eval(data.split(" = ")[1]);
-    var versionNames = [];
-    var versionUrls = [];
-    var versionFolders = [];
-    var versionFiles = [];
-    for(var versionAdd = window.versionList.length-1; versionAdd > -1; versionAdd--){
-        versionNames.push(window.versionList[versionAdd].version);
-        versionFolders.push(window.versionList[versionAdd].folder);
-        versionFiles.push(window.versionList[versionAdd].files);
-        versionUrls.push(window.versionList[versionAdd].url);
-        var versionAddElement = document.createElement("option");
-        versionAddElement.name = window.versionList[versionAdd].version; 
-        versionAddElement.value = window.versionList[versionAdd].version;
-        versionAddElement.innerText = window.versionList[versionAdd].version;
-        versionAddElement.id = window.versionList[versionAdd].url;
-        document.getElementById("versionSelect").appendChild(versionAddElement);
-    }
-    window.versionUrls = versionUrls;
-    window.versionFiles = versionFiles;
-    window.versionFolders = versionFolders;
-    window.versionNames = versionNames;
+    fs.writeFileSync(path.join(__dirname,"versions.js"), data);
+    window.versionList = eval(fs.readFileSync(path.join(__dirname, "/versions.js"), "utf8").split(" = ")[1]);
+    loadVersions();
 }
 if(navigator.onLine){
+    checkUpdates();
     getVersions();
 }else{
     window.versionList = eval(fs.readFileSync(__dirname + "/versions.js", "utf8").split(" = ")[1]);
-    var versionNames = [];
-    var versionUrls = [];
-    var versionFolders = [];
-    var versionFiles = [];
-    for(var versionAdd = window.versionList.length-1; versionAdd > -1; versionAdd--){
-        versionNames.push(window.versionList[versionAdd].version);
-        versionFolders.push(window.versionList[versionAdd].folder);
-        versionFiles.push(window.versionList[versionAdd].files);
-        versionUrls.push(window.versionList[versionAdd].url);
-        var versionAddElement = document.createElement("option");
-        versionAddElement.name = window.versionList[versionAdd].version; 
-        versionAddElement.value = window.versionList[versionAdd].version;
-        versionAddElement.innerText = window.versionList[versionAdd].version;
-        versionAddElement.id = window.versionList[versionAdd].url;
-        document.getElementById("versionSelect").appendChild(versionAddElement);
-    }
-    window.versionUrls = versionUrls;
-    window.versionFiles = versionFiles;
-    window.versionFolders = versionFolders;
-    window.versionNames = versionNames;
+    loadVersions();
 }
 async function play(select){
-    if(gettingGame !== true){
+    if(gettingGame !== true && !!document.getElementById("fileDownloadStatus") === false){
     var gettingGame = true;
     var selection = select;
     window.selection = selection;
@@ -122,28 +138,43 @@ async function play(select){
             }
             }catch(err){
                 var response = await fetch(url + "/" + filePath);
-            if(filePath.split(".")[1] == "mp3"){
-                var reader = new FileReader();
-                reader.onload = function() {
-                    var dataSave = this.result;
-                    var data = new Uint8Array(dataSave);
+                if(filePath.split(".")[1] == "mp3"){
+                    var reader = new FileReader();
+                    reader.onload = function() {
+                        var dataSave = this.result;
+                        var data = new Uint8Array(dataSave);
+                        fs.writeFileSync(filename + filePath.replace(/%20/g, " "), data);
+                    };
+                    var arrayBuffer = await response.arrayBuffer();
+                    var blob = new Blob([arrayBuffer], {type: 'audio/mpeg'});
+                    reader.readAsArrayBuffer(blob);
+                }else if(filePath.split(".")[1] == "ico"){
+                    var reader = new FileReader();
+                    reader.onload = function() {
+                        var dataSave = this.result;
+                        var data = new Uint8Array(dataSave);
+                        fs.writeFileSync(filename + filePath.replace(/%20/g, " "), data);
+                    };
+                    var arrayBuffer = await response.arrayBuffer();
+                    var blob = new Blob([arrayBuffer], {type: 'image/x-icon'});
+                    reader.readAsArrayBuffer(blob);
+                }else{
+                    var data = await response.text();
                     fs.writeFileSync(filename + filePath.replace(/%20/g, " "), data);
-                };
-                var arrayBuffer = await response.arrayBuffer();
-                var blob = new Blob([arrayBuffer], {type: 'audio/mpeg'});
-                reader.readAsArrayBuffer(blob);
-            }else{
-                var data = await response.text();
-                fs.writeFileSync(filename + filePath.replace(/%20/g, " "), data);
+                }
             }
-            }
-            window.filesDownloaded+=0.5;
-            document.getElementById("fileDownloadStatus").value = /*document.getElementById("fileDownloadStatus").value + Math.round(100*(0.5/window.versionFiles[window.versionNames.indexOf(selection.name)].length));*/100*(window.filesDownloaded/window.versionFiles[window.versionNames.indexOf(selection.name)].length);
+            window.filesDownloaded+=0.25;
+            document.getElementById("fileDownloadStatus").value = 100*(window.filesDownloaded/window.versionFiles[window.versionNames.indexOf(selection.name)].length);
+            ipcRenderer.send('loadStatus', document.getElementById("fileDownloadStatus").value/100);
+            document.body.style.cursor = "progress";
         }
         if(interval !== ""){
             var interval = setInterval(check, 10);
         }
         if(100*(window.filesDownloaded/window.versionFiles[window.versionNames.indexOf(selection.name)].length) == 100){
+            try{
+                ipcRenderer.send('loadStatus', 0-1);
+            }catch(err){}
         var progressRemove = document.getElementById("fileDownloadStatus");
         progressRemove.remove();
         const os = require('os');
@@ -162,11 +193,19 @@ async function play(select){
         }
         window.electronPath = path.join("electron", "electron-v25.0.1-" + process.platform + "-" + os.arch, "electron" + appFileType);
         if(appFileType == undefined){alert("Sorry, this version has not been made compatible for your Operating System. It is currently compatible for Debian variations of Linux, and Windows 10 and above.")}else{
+            if(process.execPath.split('\\')[process.execPath.split('\\').length-1] == "electron.exe"){
+                const { spawn } = require('child_process');
+                const child = spawn(process.execPath, [path.join(__dirname, window.versionFolders[window.versionNames.indexOf(selection.name)], "main.js")]);
+            }else{
         const { spawn } = require('child_process');
         const child = spawn(path.join(__dirname, window.electronPath), [path.join(__dirname, window.versionFolders[window.versionNames.indexOf(selection.name)], "main.js")]);
         }
         }
+        document.body.style.cursor = "default";
         }
+        }
+        getFiles();
+        getFiles();
         getFiles();
         getFiles();
     }else if((fs.existsSync(__dirname + "/" + window.versionFolders[window.versionNames.indexOf(selection.name)]))){
